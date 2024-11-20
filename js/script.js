a10c6b636d5aa49c7d845a5dc68809e0996ac103
Sócrates da Conceição Chitata Muquepe <socrates.muquepe@minfin.gov.ao>
2024-11-20 18:36:59 +0100

    closes #1 - Estrutura HTML com BS

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//fetch("https://api.imgflip.com/get_memes").then((response)=>{console.log(response.json())});
+
+$.getJSON('https://api.imgflip.com/get_memes', 'GET', (response) => {
+    for (var m of response.data.memes) {
+        document.getElementById("memes").innerHTML += 
+        `
+        <div class="card m-auto mt-4 p-3 pt-1" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">
+                    ${m.name}
+                </h5>
+            </div>
+            <img src=${m.url} class="card-img-top shadow rounded" alt="meme.">
+        </div>
+        `;
+    }
+});
