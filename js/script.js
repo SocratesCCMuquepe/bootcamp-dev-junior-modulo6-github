dc50668d44748332c8ed26403e4f1aa47917919f
Sócrates da Conceição Chitata Muquepe <socrates.muquepe@minfin.gov.ao>
2024-11-20 19:30:26 +0100

    closes #2 - Pesquisar um usuário pelo GitHub

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
 //fetch("https://api.imgflip.com/get_memes").then((response)=>{console.log(response.json())});
 
-$.getJSON('https://api.imgflip.com/get_memes', 'GET', (response) => {
-    for (var m of response.data.memes) {
-        document.getElementById("memes").innerHTML += 
-        `
-        <div class="card m-auto mt-4 p-3 pt-1" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">
-                    ${m.name}
-                </h5>
-            </div>
-            <img src=${m.url} class="card-img-top shadow rounded" alt="meme.">
-        </div>
-        `;
-    }
-});
+function search() {
+    var username = document.getElementById("username").value;
+
+    var url = `https://api.github.com/users/${username}`;
+    var method = 'GET';
+
+    $.getJSON(url, method, (user) => {
+        document.getElementById("name").innerHTML = user.name;
+        document.getElementById("html_url").innerHTML = user.html_url;
+        document.getElementById("company").innerHTML = user.company;
+        document.getElementById("avatar_url").innerHTML = `
+        <img src="${user.avatar_url}" alt="avatar" width="200" height="200" class="shadow rounded">`;
+    });
+}
