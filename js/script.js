b424ccb7b8f50f9bcde15cc33050c7158482461b
Socrates Muquepe <socycrates@hotmail.com>
2024-11-24 23:08:30 +0100

    closes #3 - Tratar o erro 404

--- js/script.js
+++ js/script.js
@@ -7,10 +7,31 @@ function search() {
     var method = 'GET';
 
     $.getJSON(url, method, (user) => {
-        document.getElementById("name").innerHTML = user.name;
-        document.getElementById("html_url").innerHTML = user.html_url;
-        document.getElementById("company").innerHTML = user.company;
-        document.getElementById("avatar_url").innerHTML = `
-        <img src="${user.avatar_url}" alt="avatar" width="200" height="200" class="shadow rounded">`;
+        showUserData(user);
+        cleanError();
+    }).fail(() => {
+        // TODO FIx this
+        showUserData({});
+        showError("Não encontrado!")
     });
+    function showError(msg) {
+        document.getElementById("error").innerHTML = `<div class="alert alert-danger" role="alert">${msg}</div>`;
+    }
+
+    function cleanError() {
+        document.getElementById("error").innerHTML = "";
+    }
+
+    function showUserData(user) {
+        document.getElementById("name").innerHTML = user.name || "";
+        document.getElementById("html_url").innerHTML = user.html_url || "";;
+        document.getElementById("company").innerHTML = user.company || "";;
+        document.getElementById("avatar_url").innerHTML = user.avatar_url ?
+            `
+                            <img src="${user.avatar_url}" alt="avatar" width="200" height="200" class="shadow rounded">
+                        ` : "";
+
+
+
+    }
 }
